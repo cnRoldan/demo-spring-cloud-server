b4da9a9a3934acf5d4e7092a62cc930b38772a7f
cnroldan <cnroldan@futurespace.es>
2025-06-23 12:46:46 +0200

    commit init

---
+++ artefacto2/src/main/java/com/example/Artefacto2Application.java
@@ -0,0 +1,12 @@
+package com.example;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Artefacto2Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Artefacto2Application.class, args);
+    }
+}
